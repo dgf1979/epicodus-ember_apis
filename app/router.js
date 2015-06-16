1a850a86a6313be8590bfad49ec636faaf896cf9
Drew Finstrom & Kim Dang <student@epicodus.com>
2015-06-16 15:29:18 -0700

    Fix columns on committees page. Add sponsor contact info on senate bills page.

--- app/router.js
+++ app/router.js
@@ -10,11 +10,15 @@ Router.map(function() {
     this.resource('contact', { path: 'contact/:zip' });
   });
   this.resource('upcoming-bills', function() {
-    this.resource('bill-details', { path: 'bill-details/:bill_id' }, function() {
+    this.resource('bill-details', { path: 'bill-details/:bill_id/' }, function() {
       this.resource('committee-details', { path: 'committee-details/:committee_id' });
     });
   });
-  this.resource('upcoming-senate-bills');
+  this.resource('upcoming-senate-bills', function() {
+    this.resource('bill-senate-details', { path: 'bill-senate-details/:bill_id/:session/:state' }, function () {
+      this.resource('senate-contact', { path: 'senate-contact/:leg_id'});
+    });
+  });
   this.resource('committees', function() {
     this.resource('house-committees', function() {
       this.resource('subcommittees', { path: 'subcommittee/:committee_id' });
