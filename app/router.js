ecdb18c783abd5d1693aa710ea7d57472251b138
Drew Finstrom & Kim Dang <student@epicodus.com>
2015-06-16 09:41:46 -0700

    add initial project files; add legislator lookup

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('legislators', { path: '/' }, function() {
+    this.resource('contact', { path: 'contact/:zip' });
+  });
 });
 
 export default Router;
